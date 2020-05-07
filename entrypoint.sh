0d6d85806877c1fbef056e6202fbb05f918b0e16
Alex Norell <alex.norell@gmail.com>
2020-05-07 00:41:47 -0700

    Return terraform diff output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,1 @@
-if [[ -n "$(terraform fmt -check -recursive)" ]]; then
-  echo "Some terraform files need be formatted, run 'terraform fmt' to fix";
-  exit 1;
-fi
+terraform fmt -check -recursive -diff
